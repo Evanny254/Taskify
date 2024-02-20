d18a29087f34821c480b481757a6ec18c93fc7be
JOY254 <nyorgijoy@gmail.com>
2024-02-20 13:47:59 -0600

    fetching user details

--- src/components/AccountDetails.jsx
+++ src/components/AccountDetails.jsx
@@ -8,3 +8,30 @@ const AccountDetails = () => {
     email: '',
     bio: ''
   });
+
+  useEffect(() => {
+    
+    const accessToken = localStorage.getItem('access_token'); 
+    fetch('https://taskify-8h37.onrender.com/user', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`  
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setUser(data);
+        setFormData({
+          username: data.username,
+          email: data.email,
+          bio: data.bio
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching user details:', error);
+      });
+  }, []);
