5d9538644c7bcd846d972869b2c2a0b3e2f195a9
Evanny254 <evancherry254@gmail.com>
2024-02-20 22:10:22 +0300

    Create App Function & Render

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,26 @@ import ProjectForm from './components/ProjectForm';
 import AccountDetails from './components/AccountDetails';
 import AuthHandler from './components/AuthHandler'; // Import the AuthHandler component
 import './App.css';
+
+function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const accessToken = localStorage.getItem('access_token'); // Retrieve the token from local storage
+        const response = await fetch('https://taskify-8h37.onrender.com/user', {
+          headers: {
+            'Authorization': `Bearer ${accessToken}` // Include the token in the Authorization header
+          }
+        });
+        if (response.ok) {
+          const userData = await response.json();
+          setUser(userData);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+    fetchUser();
+  }, []);
