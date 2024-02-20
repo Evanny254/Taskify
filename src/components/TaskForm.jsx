13090326c844b2de89fe8dff7fcb3ecea0510197
JOY254 <nyorgijoy@gmail.com>
2024-02-20 08:28:59 -0600

    refactor

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,3 +2,8 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import TaskActions from "./TaskActions";
+
+const TaskForm = () => {
+    const initialFormData = { /* ... */ };
+    const [formData, setFormData] = useState({ ...initialFormData });
+  
