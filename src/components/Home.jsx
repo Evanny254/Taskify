c9ec6980bd02dfdbbae5bd4967563980b58c3875
chris-ouko <144042167+chris-ouko@users.noreply.github.com>
2024-02-20 16:56:06 +0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,3 +5,14 @@ const Home = ({ user, onSignOut }) => {
   if (!user) {
     return <div>Welcome to Taskify</div>; 
   }
+return (
+    <div className='container'>
+      <h1>Welcome, {user.username}!</h1>
+      <Link to="/tasklist">View Tasks</Link>
+      <Link to="/projectlist">View Projects</Link>
+      <button onClick={onSignOut}>Sign Out</button>
+    </div>
+  );
+};
+
+export default Home;
