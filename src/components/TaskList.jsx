91f1c6094b593e39b8345a700a9bc1e8ba084c9f
JOY254 <nyorgijoy@gmail.com>
2024-02-20 09:42:25 -0600

    fetch tasks

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -6,4 +6,17 @@ const TaskList = () => {
 const [selectedTask, setSelectedTask] = useState(null);
 const [commentInput, setCommentInput] = useState('');
 const [comments, setComments] = useState({});
+useEffect(() => {
+    // Fetch tasks from the specified API
+    fetch('https://taskify-8h37.onrender.com/tasks')
+      .then(response => response.json())
+      .then(data => {
+        // Update the 'tasks' state with the fetched data
+        setTasks(data);
+      })
+      .catch(error => {
+        console.error('Error fetching tasks:', error);
+      });
+  }, []);
+  
 
