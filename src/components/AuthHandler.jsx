515d55458b614f0ae739fc6a65ce8784f697ed56
JOY254 <nyorgijoy@gmail.com>
2024-02-20 14:03:09 -0600

    adding handle login functionality

--- src/components/AuthHandler.jsx
+++ src/components/AuthHandler.jsx
@@ -6,3 +6,24 @@ import Home from './Home';
 
 function AuthHandler({ user, setUser }) {
     const navigate = useNavigate();
+
+    const handleLogin = async (username, password) => {
+        try {
+            const response = await fetch('https://taskify-8h37.onrender.com/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            });
+            if (response.ok) {
+                const userData = await response.json();
+                setUser(userData);
+                navigate('/home'); // Redirect to home after successful login
+            } else {
+                console.error('Sign in failed:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Sign in failed:', error);
+        }
+    };
