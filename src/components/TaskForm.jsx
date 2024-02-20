699e6f89e7898382d3f8bf7672fb9d65cff4c0e0
JOY254 <nyorgijoy@gmail.com>
2024-02-20 08:32:47 -0600

    add UI element

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -21,6 +21,32 @@ const TaskForm = () => {
           [name]: name === "due_date" || name === "reminder_date" ? (value ? new Date(value) : null) : value,
         }));
       };
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        const taskData = { /* ... */ };
+      
+        try {
+          const response = await fetch("https://taskify-8h37.onrender.com/tasks", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(taskData),
+          });
+      
+          if (response.ok) {
+            console.log("Task created successfully.");
+            setFormData({ ...initialFormData });
+            alert("Task Submitted Successfully");
+          } else {
+            console.error(`Task creation failed with status: ${response.status}`);
+          }
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      };
+      
       
 
 
