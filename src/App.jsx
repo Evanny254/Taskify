e00994b5bf9d13891cedd712b834d5467651330e
Evanny254 <evancherry254@gmail.com>
2024-02-20 22:11:30 +0300

    Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -33,3 +33,22 @@ function App() {
     };
     fetchUser();
   }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={user ? <AuthHandler user={user}><Home user={user} /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="/tasklist" element={user ? <AuthHandler user={user}><TaskList /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="/projectlist" element={user ? <AuthHandler user={user}><ProjectList /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="/taskform" element={user ? <AuthHandler user={user}><TaskForm /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="/projectform" element={user ? <AuthHandler user={user}><ProjectForm /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="/account" element={user ? <AuthHandler user={user}><AccountDetails user={user} /></AuthHandler> : <Navigate to="/signin" />} />
+        <Route path="*" element={<Navigate to="/signin" />} /> {/* Catch-all route to redirect to sign-in */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
