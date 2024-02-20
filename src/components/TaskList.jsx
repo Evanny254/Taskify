4a24051d9fa5e3273936e726c4aec05e5f0582f2
JOY254 <nyorgijoy@gmail.com>
2024-02-20 09:41:36 -0600

    declare state variables

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,3 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const TaskList = () => {
+    const [tasks, setTasks] = useState([]);
+const [selectedTask, setSelectedTask] = useState(null);
+const [commentInput, setCommentInput] = useState('');
+const [comments, setComments] = useState({});
+
