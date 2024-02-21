6eba9c3a41109e13a25817774953cfaf348080d1
morrison <morrisonwanja@gmail.com>
2024-02-21 08:10:06 +0300

    Add submit and signup functions

--- src/Componets/SingUp.jsx
+++ src/Componets/SingUp.jsx
@@ -2,3 +2,23 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+
+const SignUp = ({ handleRegister }) => {
+  const navigate = useNavigate();
+  const { 
+    formState: { errors, isSubmitting },
+    handleSubmit, // Define handleSubmit here
+    register 
+  } = useForm(); // Initialize useForm
+
+  const onSubmit = async (formData) => {
+    try {
+      await handleRegister(formData.username, formData.email, formData.password);
+      navigate('/signin'); // Assuming 'navigate' is defined elsewhere
+    } catch (err) {
+      if (err.response?.status === 409) {
+        toast.error('Account already created.');
+      }
+    }
+  };
+
