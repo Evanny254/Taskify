1120ad3779a8deb882bae51be1eee312c04bd61d
morrison <morrisonwanja@gmail.com>
2024-02-22 21:47:40 +0300

    Review task list

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,155 +1,127 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
 
 const TaskList = () => {
   const [tasks, setTasks] = useState([]);
   const [selectedTask, setSelectedTask] = useState(null);
-  const [commentInput, setCommentInput] = useState("");
   const [comments, setComments] = useState({});
+
   useEffect(() => {
-    // Fetch tasks from the specified API
-    const storedToken = localStorage.getItem("accessToken");
-    fetch("https://taskify-backend-btvr.onrender.com/tasks")
-      .then((response) => response.json())
-      .then((data) => {
-        // Update the 'tasks' state with the fetched data
-        console.log(data);
+    const fetchTasks = async () => {
+      try {
+        const accessToken = localStorage.getItem("access_token");
+        const response = await fetch(
+          "https://taskify-backend-btvr.onrender.com/tasks",
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch tasks");
+        }
+        const data = await response.json();
+        console.log("Fetched tasks:", data);
         setTasks(data);
-      })
-      .catch((error) => {
+      } catch (error) {
         console.error("Error fetching tasks:", error);
-      });
+      }
+    };
+
+    fetchTasks();
   }, []);
-  const fetchComments = (taskId) => {
-    // Fetch comments for a specific task
-    fetch(`https://taskify-backend-btvr.onrender.com/comments/${taskId}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Update the 'comments' state with the fetched data
-        setComments((prevState) => ({
-          ...prevState,
-          [taskId]: data,
-        }));
-      })
-      .catch((error) => {
-        console.error("Error fetching comments:", error);
-      });
+
+  const fetchComments = async (taskId) => {
+    try {
+      const response = await fetch(
+        `https://taskify-backend-btvr.onrender.com/comments/${taskId}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch comments");
+      }
+      const data = await response.json();
+      console.log("Fetched comments for task:", taskId, data);
+      setComments((prevState) => ({
+        ...prevState,
+        [taskId]: data,
+      }));
+    } catch (error) {
+      console.error("Error fetching comments:", error);
+    }
   };
 
   const handleTaskClick = (taskId) => {
     if (selectedTask === taskId) {
       setSelectedTask(null);
     } else {
-      // Set the selected task and fetch comments for it
       setSelectedTask(taskId);
       fetchComments(taskId);
     }
   };
 
-  const handleCommentChange = (e) => {
-    // Update the 'commentInput' state as the user types
-    setCommentInput(e.target.value);
-  };
-
-  const handleCommentSubmit = (taskId) => {
-    // Send a POST request to add a new comment to a specific task
-    fetch(
-      `https://taskify-backend-btvr.onrender.com/tasks/comments/${taskId}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ comment: commentInput }),
+  const handleCommentSubmit = async (taskId, comment) => {
+    try {
+      const response = await fetch(
+        `https://taskify-backend-btvr.onrender.com/tasks/comments/${taskId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ comment }),
+        }
+      );
+      if (!response.ok) {
+        throw new Error("Failed to submit comment");
       }
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        // Update the 'comments' state with the new comment
-        setComments((prevState) => ({
-          ...prevState,
-          [taskId]: [...prevState[taskId], data],
-        }));
-        // Clear the comment input field
-        setCommentInput("");
-      })
-      .catch((error) => {
-        console.error("Error submitting comment:", error);
-      });
+      const data = await response.json();
+      console.log("Submitted comment:", data);
+      setComments((prevState) => ({
+        ...prevState,
+        [taskId]: [...prevState[taskId], data],
+      }));
+    } catch (error) {
+      console.error("Error submitting comment:", error);
+    }
   };
 
-  const handleDeleteComment = (taskId, commentId) => {
-    // Send a DELETE request to remove a comment from a specific task
-    fetch(
-      `https://taskify-backend-btvr.onrender.com/tasks/${taskId}/comments/${commentId}`,
-      {
+  const handleDeleteComment = async (taskId, commentId) => {
+    try {
+      await fetch(
+        `https://taskify-backend-btvr.onrender.com/tasks/${taskId}/comments/${commentId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      setComments((prevState) => ({
+        ...prevState,
+        [taskId]: prevState[taskId].filter(
+          (comment) => comment.id !== commentId
+        ),
+      }));
+    } catch (error) {
+      console.error("Error deleting comment:", error);
+    }
+  };
+
+  const handleDeleteTask = async (taskId) => {
+    try {
+      await fetch(`https://taskify-backend-btvr.onrender.com/tasks/${taskId}`, {
         method: "DELETE",
-      }
-    )
-      .then(() => {
-        // Update the 'comments' state by removing the deleted comment
-        setComments((prevState) => ({
-          ...prevState,
-          [taskId]: prevState[taskId].filter(
-            (comment) => comment.id !== commentId
-          ),
-        }));
-      })
-      .catch((error) => {
-        console.error("Error deleting comment:", error);
       });
-  };
-
-  const handleDeleteTask = (taskId) => {
-    // Send a DELETE request to remove a task
-    fetch(`https://taskify-backend-btvr.onrender.com/tasks/${taskId}`, {
-      method: "DELETE",
-    })
-      .then(() => {
-        // Update the 'tasks' state by removing the deleted task
-        setTasks(tasks.filter((task) => task.id !== taskId));
-      })
-      .catch((error) => {
-        console.error("Error deleting task:", error);
-      });
-  };
-
-  const handleUpdateTask = (taskId, updatedTaskData) => {
-    // Send a PUT request to update a task with new data
-    fetch(`https://taskify-backend-btvr.onrender.com/${taskId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updatedTaskData),
-    })
-      .then(() => {
-        // Update the 'tasks' state with the updated task data
-        const updatedTasks = tasks.map((task) => {
-          if (task.id === taskId) {
-            return { ...task, ...updatedTaskData };
-          }
-          return task;
-        });
-        setTasks(updatedTasks);
-      })
-      .catch((error) => {
-        console.error("Error updating task:", error);
-      });
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    } catch (error) {
+      console.error("Error deleting task:", error);
+    }
   };
 
   return (
     <div className="container">
       <h2>Task List</h2>
       {tasks.map((task) => (
-        <div
-          key={task.id}
-          style={{
-            marginBottom: "20px",
-            border: "1px solid black",
-            padding: "10px",
-          }}
-        >
+        <div key={task.id}>
           <h3
             onClick={() => handleTaskClick(task.id)}
             style={{ cursor: "pointer" }}
@@ -177,14 +149,18 @@ const TaskList = () => {
                     </button>
                   </div>
                 ))}
-              <input
-                type="text"
-                value={commentInput}
-                onChange={handleCommentChange}
-              />
-              <button onClick={() => handleCommentSubmit(task.id)}>
-                Add Comment
-              </button>
+              <Formik
+                initialValues={{ comment: "" }}
+                onSubmit={(values, { resetForm }) => {
+                  handleCommentSubmit(task.id, values.comment);
+                  resetForm();
+                }}
+              >
+                <Form>
+                  <Field type="text" name="comment" />
+                  <button type="submit">Add Comment</button>
+                </Form>
+              </Formik>
               <button onClick={() => handleDeleteTask(task.id)}>
                 Delete Task
               </button>
