a7363b3ea6111db9783fd18192a1d6eaeb7fcb7d
Evanny254 <evancherry254@gmail.com>
2024-02-27 10:57:59 +0300

    Add signout Feature

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -37,7 +37,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <Footer className="flex-shrink-0" /> {/* Include the Footer component with flex-shrink-0 */}
+      <Footer className="flex-shrink-0" />
     </div>
   );
 };
