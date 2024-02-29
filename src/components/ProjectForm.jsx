cdbaca6bf71a1809c591d0b5ad853fac42d22876
morrison <morrisonwanja@gmail.com>
2024-02-29 18:28:48 +0300

    Add from notifications

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -24,57 +24,60 @@ const ProjectForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-      // Format dates before sending to the server
-      const formatDate = (date) => {
-        if (!date) return null;
-        const d = new Date(date);
-        const year = d.getFullYear();
-        const month = String(d.getMonth() + 1).padStart(2, "0");
-        const day = String(d.getDate()).padStart(2, "0");
-        return `${year}-${month}-${day}`;
-      };
+    // Format dates before sending to the server
+    const formatDate = (date) => {
+      if (!date) return null;
+      const d = new Date(date);
+      const year = d.getFullYear();
+      const month = String(d.getMonth() + 1).padStart(2, "0");
+      const day = String(d.getDate()).padStart(2, "0");
+      return `${year}-${month}-${day}`;
+    };
 
-      const formattedStartDate = formatDate(formData.start_date);
-      const formattedEndDate = formatDate(formData.end_date);
+    const formattedStartDate = formatDate(formData.start_date);
+    const formattedEndDate = formatDate(formData.end_date);
 
-      const projectData = {
-        name: formData.name,
-        description: formData.description,
-        start_date: formattedStartDate, 
-        end_date: formattedEndDate, 
-      };
-      
-      const accessToken = localStorage.getItem('access_token');
-      console.log(projectData)
-       fetch("https://taskify-backend-5v37.onrender.com/projects", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify(projectData),
-          })
+    const projectData = {
+      name: formData.name,
+      description: formData.description,
+      start_date: formattedStartDate,
+      end_date: formattedEndDate,
+    };
 
-          .then((response) => {
-            if (response.status ===  201) {
-              console.log("Project created successfully.");
-              setFormData({ ...initialFormData });  
-            } else {
-              console.error("Project creation failed.");
-            }
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-          });
-      };
+    const accessToken = localStorage.getItem("access_token");
+    console.log(projectData);
+    fetch("https://taskify-backend-5v37.onrender.com/projects", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(projectData),
+    })
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("Project created successfully.");
+          setFormData({ ...initialFormData });
+        } else {
+          console.error("Project creation failed.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
   return (
     <div className="container mx-auto mt-8">
       <div className="bg-cyan-100 p-8 rounded-md shadow-lg">
-        <h2 className="text-2xl font-semibold mb-4 text-cyan-800">Create a Project</h2>
+        <h2 className="text-2xl font-semibold mb-4 text-cyan-800">
+          Create a Project
+        </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-cyan-700 font-medium mb-2">Name:</label>
+            <label className="block text-cyan-700 font-medium mb-2">
+              Name:
+            </label>
             <input
               type="text"
               name="name"
@@ -85,7 +88,9 @@ const ProjectForm = () => {
             />
           </div>
           <div className="mb-4">
-            <label className="block text-cyan-700 font-medium mb-2">Description:</label>
+            <label className="block text-cyan-700 font-medium mb-2">
+              Description:
+            </label>
             <textarea
               type="text"
               name="description"
@@ -96,27 +101,35 @@ const ProjectForm = () => {
             />
           </div>
           <div className="mb-4">
-              <label className="block text-cyan-700 font-medium mb-2">Start Date:</label>
-              <DatePicker
-                selected={formData.start_date}
-                onChange={(selectedDate) =>
-                  handleChange({ target: { name: "start_date", value: selectedDate } })
-                }
-                className="block w-full border border-cyan-300 rounded-md py-2 px-3 focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-cyan-700 font-medium mb-2">End Date:</label>
-              <DatePicker
-                selected={formData.end_date}
-                onChange={(selectedDate) =>
-                  handleChange({ target: { name: "end_date", value: selectedDate } })
-                }
-                className="block w-full border border-cyan-300 rounded-md py-2 px-3 focus:outline-none"
-                required
-              />
-            </div>
+            <label className="block text-cyan-700 font-medium mb-2">
+              Start Date:
+            </label>
+            <DatePicker
+              selected={formData.start_date}
+              onChange={(selectedDate) =>
+                handleChange({
+                  target: { name: "start_date", value: selectedDate },
+                })
+              }
+              className="block w-full border border-cyan-300 rounded-md py-2 px-3 focus:outline-none"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-cyan-700 font-medium mb-2">
+              End Date:
+            </label>
+            <DatePicker
+              selected={formData.end_date}
+              onChange={(selectedDate) =>
+                handleChange({
+                  target: { name: "end_date", value: selectedDate },
+                })
+              }
+              className="block w-full border border-cyan-300 rounded-md py-2 px-3 focus:outline-none"
+              required
+            />
+          </div>
           <button
             className="bg-cyan-500 hover:bg-cyan-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             type="submit"
